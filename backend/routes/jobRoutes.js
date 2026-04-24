1ce82a4e07abd5236e7f44fd6e5eb3f206067442
Tawfeeq <tawfeeqsulai@gmail.com>
2026-04-25 01:08:33 +0530

    connected frontend to backend

--- backend/routes/jobRoutes.js
+++ backend/routes/jobRoutes.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const Job = require("../models/Job");
+const Job = require("../models/job");
 
 // 🔐 AUTH MIDDLEWARE
 function auth(req, res, next) {
